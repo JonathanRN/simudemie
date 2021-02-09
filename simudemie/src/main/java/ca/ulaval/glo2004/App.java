26a15ef3da7c768792d8e6891bdbb9c38b91a1d5
Frederick Hughes <frederick.hughes.1@ulaval.ca>
2021-02-09 18:18:50 -0500

    Update App.java
    Code de Anthony sur les distributions binomiales

--- simudemie/src/main/java/ca/ulaval/glo2004/App.java
+++ simudemie/src/main/java/ca/ulaval/glo2004/App.java
@@ -17,6 +17,47 @@ public class App {
     // Vous n'aurez pas besoin d'ecrire tout ce code, il sera genere automatiquement par intellij ou netbeans
     // Par contre vous aurez a creer les actions listener pour vos boutons et etc.
     public static void main(String[] args) {
+        /*
+        Random rnd = new Random (System.currentTimeMillis());
+        double seuilTol = 0.0001;
+        
+        BinomialDistribution binomial = new BinomialDistribution(100, 0.05);
+
+        
+        ArrayList<Double> probabilites = new ArrayList<>();
+        ArrayList<Integer> nombreSucces = new ArrayList<>();
+        
+        int x = 5;
+        double prob = binomial.probability(x);
+        while(prob > seuilTol && x >= 0){
+            probabilites.add(prob);
+            nombreSucces.add(x);
+            x -= 1;
+            prob = binomial.probability(x);
+        }
+        
+        System.out.println(probabilites);
+        System.out.println(nombreSucces);
+        
+        double randomNumber = 0.0;
+        double current = 0.0; //treshold
+        int success = 0;
+        
+        for (int k = 0; k < 10; k++){
+            randomNumber = rnd.nextDouble();
+            current = 0.0; //treshold
+            success = 0;
+            for(int i = 0; i < probabilites.size(); i++){
+                current += probabilites.get(0);
+                if(randomNumber < current){
+                    success = nombreSucces.get(i);
+                    break;
+                }
+            }
+        }
+        System.out.println(success);
+        */
+        
         // Creation du main window
         JFrame frame = new JFrame();
 
@@ -53,6 +94,7 @@ public class App {
 
         // Simple commande permettant d'ecrire sur la console
         System.out.println("hello world");
+
     }
 }
 

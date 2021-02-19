60a0297cd61f894248ccc13e42d9c23d8fdbebc7
henri.bernard.stl <HEBES14@ulaval.ca>
2021-02-18 23:23:36 -0500

    TabIcons

--- simudemie/src/main/java/ca/ulaval/glo2004/MenuPrincipal.java
+++ simudemie/src/main/java/ca/ulaval/glo2004/MenuPrincipal.java
@@ -59,6 +59,10 @@ public class MenuPrincipal extends javax.swing.JFrame {
         Layout = new ca.ulaval.glo2004.RoundedPanel();
         scenarioMapPanel1 = new ca.ulaval.glo2004.ScenarioMapPanel();
         scenarioStatsPanel1 = new ca.ulaval.glo2004.ScenarioStatsPanel();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jPanel5 = new javax.swing.JPanel();
         TitleMenuBar = new javax.swing.JMenuBar();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -90,7 +94,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
             .addGap(0, 594, Short.MAX_VALUE)
         );
 
-        TabMenu.addTab("", new javax.swing.ImageIcon(getClass().getResource("/icons/Logo3.130.png")), TitleMenu, "Accueil"); // NOI18N
+        TabMenu.addTab("", new javax.swing.ImageIcon(getClass().getResource("/icons/Logo3.150.png")), TitleMenu, "Accueil"); // NOI18N
 
         ScenariosTab.setBackground(new java.awt.Color(46, 52, 64));
         ScenariosTab.setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 50, 35, 35));
@@ -193,6 +197,58 @@ public class MenuPrincipal extends javax.swing.JFrame {
 
         TabMenu.addTab("", new javax.swing.ImageIcon(getClass().getResource("/icons/icons8_play_50px.png")), ScenariosTab, "Scénarios"); // NOI18N
 
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1059, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 594, Short.MAX_VALUE)
+        );
+
+        TabMenu.addTab("", new javax.swing.ImageIcon(getClass().getResource("/icons/icons8_map_50px_1.png")), jPanel1); // NOI18N
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1059, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 594, Short.MAX_VALUE)
+        );
+
+        TabMenu.addTab("", new javax.swing.ImageIcon(getClass().getResource("/icons/icons8_coronavirus_50px_1.png")), jPanel2); // NOI18N
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1059, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 594, Short.MAX_VALUE)
+        );
+
+        TabMenu.addTab("", new javax.swing.ImageIcon(getClass().getResource("/icons/icons8_gallery_50px_1.png")), jPanel3); // NOI18N
+
+        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
+        jPanel5.setLayout(jPanel5Layout);
+        jPanel5Layout.setHorizontalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1059, Short.MAX_VALUE)
+        );
+        jPanel5Layout.setVerticalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 594, Short.MAX_VALUE)
+        );
+
+        TabMenu.addTab("", new javax.swing.ImageIcon(getClass().getResource("/icons/icons8_exit_50px_2.png")), jPanel5); // NOI18N
+
         MainMenu.add(TabMenu, java.awt.BorderLayout.CENTER);
 
         getContentPane().add(MainMenu, java.awt.BorderLayout.CENTER);
@@ -241,6 +297,10 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JTabbedPane TabMenu;
     private javax.swing.JPanel TitleMenu;
     private javax.swing.JMenuBar TitleMenuBar;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel5;
     private ca.ulaval.glo2004.ScenarioMapPanel scenarioMapPanel1;
     private ca.ulaval.glo2004.ScenarioStatsPanel scenarioStatsPanel1;
     // End of variables declaration//GEN-END:variables

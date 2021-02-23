9206c619d8f3dd133f2d03c51a9cd70682536642
Frederick Hughes <frederick.hughes.1@ulaval.ca>
2021-02-22 20:08:29 -0500

    TODO de la classe région

--- simudemie/src/main/java/ca/ulaval/glo2004/domaine/Pays.java
+++ simudemie/src/main/java/ca/ulaval/glo2004/domaine/Pays.java
@@ -14,4 +14,41 @@ public class Pays {
     private int jour;
     
     
+    public Pays()
+    {
+        //TODO Constructeur de la classe pays
+    }
+    
+    
+    void AvancerJour()
+    {
+        //TODO Fonction qui applique tous les effets sur les populations des régions
+    }
+    
+    private void AjouterRegion(Region nouvelleRegion)
+    {
+        //TODO Ajout d'une region à la liste
+    }
+    
+     private void RetirerRegion(Region ancienneRegion)
+    {
+        //TODO Retrait d'une region à la liste
+    }
+    
+    public String getNom(){return nom;}
+    
+    public List<Region> getListePays(){return listePays;}
+      
+    public int getJour(){return jour;}
+    
+    private void setNom(String nom)
+    {
+        this.nom = nom;
+    }
+    
+    private void setJour(int jour)
+    {
+        this.jour = jour;
+    }
+    
 }
--- simudemie/src/main/java/ca/ulaval/glo2004/domaine/Region.java
+++ simudemie/src/main/java/ca/ulaval/glo2004/domaine/Region.java
@@ -5,10 +5,86 @@
  */
 package ca.ulaval.glo2004.domaine;
 
-/**
- *
- * @author Fred
- */
+import java.util.List;
+
 public class Region {
+    
+    private String nom;
+    private int populationSaine;
+    private int populationImmune;
+    private int populationInfectee;
+    private int populationDecedee;
+    private List<Region> regionVoisines;
+    
+    Region() 
+    {
+    //TODO Constructeur 
+    }
+    
+    void DeplacementRegions()
+    {
+        //TODO Déplacement des populations entre les régions
+    }
+    
+    void Contaminer()
+    {
+        //TODO Appliquer le taux d'infection sur la populationSaine
+        //Gestion des Mesures
+    }
+    
+    void EliminerPopulation()
+    {
+        //TODO Appliquer le taux de mortalite sur la populationInfectee
+    }
+    
+    void GuerirPop()
+    {
+        //TODO Appliquer le taux de guérison sur la populationInfectee
+    }
+    
+    private void AjouterVoisin(Region nouvelleRegion)
+    {
+        //TODO Ajouter une region à sa liste de voisin
+    }
+    
+    private void RetirerVoisin(Region ancienneRegion)
+    {
+           //TODO Retirer une région de la liste de region voisines
+    }
+    
+    public String getNom(){return nom;}
+    
+    public int getPopSaine(){return populationSaine;}
+    
+    public int getPopImmunisee(){return populationImmune;}
+    
+    public int getPopInfectee(){return populationInfectee;}
+    
+    public int getPopDecedee(){return populationDecedee;}
+    
+    private void setNom(String nom)
+    {
+        this.nom = nom;
+    }
+    
+    private void setPopSaine(int populationSaine)
+    {
+        this.populationSaine = populationSaine;
+    }
+    
+    private void setPopImmune(int populationImmune)
+    {
+        this.populationImmune = populationImmune;
+    }
+        
+    private void setPopInfectee(int populationInfectee)
+    {
+        this.populationInfectee = populationInfectee;
+    }
+            
+    private void setPopDecedee(int populationDecedee)
+    {
+        this.populationDecedee = populationDecedee;
+    }
     
 }

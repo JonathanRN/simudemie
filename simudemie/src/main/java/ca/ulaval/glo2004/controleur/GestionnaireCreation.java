4653b46aeda0396a1771323bca5ef939a76782da
Frederick Hughes <frederick.hughes.1@ulaval.ca>
2021-02-23 10:13:27 -0500

    TODO gestionnaire de creation

--- simudemie/src/main/java/ca/ulaval/glo2004/controleur/GestionnaireCreation.java
+++ simudemie/src/main/java/ca/ulaval/glo2004/controleur/GestionnaireCreation.java
@@ -5,10 +5,36 @@
  */
 package ca.ulaval.glo2004.controleur;
 
-/**
- *
- * @author Fred
- */
+
 public class GestionnaireCreation {
     
+    public GestionnaireCreation()
+    {
+        //TODO ??? Constructeur?
+    }
+    
+    private void CreerCarte(String nom)
+    {
+        //TODO Appel au constructeur de carte
+    }
+    
+    private void CreerPays(String nom)
+    {
+        //TODO Appel au constructeur 
+    }
+    
+    private void CreerRegion(String nom, int popSaine)
+    {
+        //TODO Appel au constructeur 
+    }
+        
+    private void CreerLiaison(enum type, Pays paysOrigine, Pays paysDestination)
+    {
+        //TODO Appel au constructeur 
+    }
+            
+    private void CreerMaladie(String nom, float tauxInf, float, tauxMort, float tauxGuerison)
+    {
+        //TODO Appel au constructeur 
+    }
 }

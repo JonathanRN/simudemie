74a392e817f62d94aa77422cc448f77eacde3450
Frederick Hughes <frederick.hughes.1@ulaval.ca>
2021-02-22 21:21:26 -0500

    TODO VoieDeLiaison
    Tout le package Domaine a été créé (classes et attributs), il ne reste qu'à implanter certaines méthodes.

--- simudemie/src/main/java/ca/ulaval/glo2004/domaine/VoieLiaison.java
+++ simudemie/src/main/java/ca/ulaval/glo2004/domaine/VoieLiaison.java
@@ -5,10 +5,52 @@
  */
 package ca.ulaval.glo2004.domaine;
 
-/**
- *
- * @author Fred
- */
+
 public class VoieLiaison {
+    
+    private enum type {
+        voieTerrestre,
+        voieMaritime,
+        voieAerienne
+    }
+    
+    private Pays paysOrigine;
+    
+    private Pays paysDestination;
+    
+    private boolean accessible;
+    
+    public VoieLiaison()
+    {
+        //TODO  Constructeur
+    }
+    
+    public String getType(){return this.toString();} //Pas certain du tout -- Est-ce utile?
+    
+    public Pays getPaysOrigine(){return paysOrigine;}
+    
+    public Pays getPaysDestination(){return paysDestination;}
+    
+    public boolean getAccessible(){return accessible;}
+        
+    private void setType(enum type) //Pas certain -- Est-ce utile?
+    {
+        this.type = type;
+    } 
+    
+    private void setPaysOrigine(Pays paysOrigine)
+    {
+        this.paysOrigine = paysOrigine;
+    }
+    
+    private void setPaysDestination(Pays paysDestination)
+    {
+        this.paysDestination = paysDestination;
+    }
+    
+    private void setAccessible(boolean accessible)
+    {
+        this.accessible = accessible;
+    }
     
 }

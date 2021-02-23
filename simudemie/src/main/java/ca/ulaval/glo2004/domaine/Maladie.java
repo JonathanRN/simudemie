d028eb3ed6d04de25edb3a2848e9f63be484ba6f
Frederick Hughes <frederick.hughes.1@ulaval.ca>
2021-02-22 20:25:43 -0500

    TODO Maladie et Mesures

--- simudemie/src/main/java/ca/ulaval/glo2004/domaine/Maladie.java
+++ simudemie/src/main/java/ca/ulaval/glo2004/domaine/Maladie.java
@@ -5,10 +5,45 @@
  */
 package ca.ulaval.glo2004.domaine;
 
-/**
- *
- * @author Fred
- */
+
 public class Maladie {
+
+    private String nom;
+    private float tauxInfection;
+    private float tauxMortalite;
+    private float tauxGuerison;
     
+    public Maladie()
+    {
+        //TODO Constructeur Maladie
+    }
+    
+    public String getNom(){return nom;}
+    
+    public float getTauxInfection(){return tauxInfection;}
+    
+    public float getTauxMortalite(){return tauxMortalite;}
+    
+    public float getTauxGuerison(){return tauxGuerison;}
+    
+    private void setNom(String nom)
+    {
+        this.nom = nom;
+    }
+    
+    private void setTauxInfection(float tauxInfection)
+    {
+        this.tauxInfection = tauxInfection;
+    }
+        
+    private void setTauxMortalite(float tauxMortalite)
+    {
+        this.tauxMortalite = tauxMortalite;
+    }
+    
+    private void setTauxGuerison(float tauxGuerison)
+    {
+        this.tauxGuerison = tauxGuerison;
+    }
+        
 }
--- simudemie/src/main/java/ca/ulaval/glo2004/domaine/Mesure.java
+++ simudemie/src/main/java/ca/ulaval/glo2004/domaine/Mesure.java
@@ -5,10 +5,36 @@
  */
 package ca.ulaval.glo2004.domaine;
 
-/**
- *
- * @author Fred
- */
+
 public class Mesure {
     
+    private String nom;
+    private float tauxAdhesion;
+    private float tauxReductionProp;
+    
+    Mesure()
+    {
+        //TODO Constructeur Mesure
+    }
+    
+    public String getNom(){return nom;}
+    
+    public float getTauxAdhesion(){return tauxAdhesion;}
+    
+    public float getTauxReductionProp(){return tauxReductionProp;}
+    
+    private void setNom(String nom)
+    {
+        this.nom = nom;
+    }
+    
+    private void setTauxAdhesion(float tauxAdhesion)
+    {
+        this.tauxAdhesion = tauxAdhesion;
+    }
+        
+    private void setTauxReductionProp(float tauxReductionProp)
+    {
+        this.tauxReductionProp = tauxReductionProp;
+    }
 }

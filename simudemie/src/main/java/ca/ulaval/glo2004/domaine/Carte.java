cf3d78c13a66200eee46e2f9af9249b43c17caf6
Frederick Hughes <frederick.hughes.1@ulaval.ca>
2021-04-02 14:47:26 -0400

    Changement Propagation InterPays + ajout PopImmune

--- simudemie/src/main/java/ca/ulaval/glo2004/domaine/Carte.java
+++ simudemie/src/main/java/ca/ulaval/glo2004/domaine/Carte.java
@@ -84,7 +84,7 @@ public class Carte implements Serializable {
             if (paysOrigine.listeRegions.get(0).getPopInfectee() > 0 && paysDestination.listeRegions.get(0).getPopInfectee() == 0 )
             {
                 double prob = Math.random();
-                if (prob < 0.01 || (prob < 0.1 && paysDestination.getPopInfectee() > 5000) || (prob < 0.3 && paysDestination.getPopInfectee() > 1000)){
+                if (prob < 0.1 || (prob < 0.3 && paysDestination.getPopInfectee() > 1000)){
                     //Selection de la region 0 (la region ou se situe la voie - "theoriquement")
                     paysDestination.listeRegions.get(0).setPopInfectee(1); 
                     paysDestination.listeRegions.get(0).setPopSaine(paysDestination.listeRegions.get(0).getPopSaine() - 1); 
@@ -94,7 +94,7 @@ public class Carte implements Serializable {
             if (paysDestination.listeRegions.get(0).getPopInfectee() > 0 && paysOrigine.listeRegions.get(0).getPopInfectee() == 0)
             {
                 double prob = Math.random();
-                if (prob < 0.01 || (prob < 0.1 && paysDestination.getPopInfectee() > 5000) || (prob < 0.3 && paysDestination.getPopInfectee() > 1000)){
+                if (prob < 0.1 || (prob < 0.3 && paysDestination.getPopInfectee() > 1000)){
                     //Selection de la region 0 (la region ou se situe la voie - "theoriquement")
                     paysOrigine.listeRegions.get(0).setPopInfectee(paysOrigine.listeRegions.get(0).getPopInfectee()+1);
                     paysOrigine.listeRegions.get(0).setPopSaine(paysOrigine.listeRegions.get(0).getPopSaine() - 1);

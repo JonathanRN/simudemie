9a0b5c2f399b99048bdbc6f9c94676a9e448c329
Michaël Vermette <michael.vermette.1@ulaval.ca>
2021-04-15 14:53:42 -0400

    Maladie: Ajout temps incubation + immunization checkbox

--- simudemie/src/main/java/ca/ulaval/glo2004/domaine/controleur/GestionnaireMaladie.java
+++ simudemie/src/main/java/ca/ulaval/glo2004/domaine/controleur/GestionnaireMaladie.java
@@ -34,17 +34,21 @@ public class GestionnaireMaladie extends GestionnaireOnglet<Maladie> {
         double tauxInf = (double) arguments[2];
         double tauxDead = (double) arguments[3];
         double tauxGue = (double) arguments[4];
+        int incubation = (int) arguments[5];
+        boolean immunitePossible = (boolean) arguments[6];
         
         Maladie maladie = getElement(index);
         
         if(maladie == null) {
-            maladie = new Maladie(nom, tauxInf, tauxDead, tauxGue);
+            maladie = new Maladie(nom, tauxInf, tauxDead, tauxGue, incubation, immunitePossible);
             ajouter(maladie);
         } else {
             maladie.setNom(nom);
             maladie.setTauxInfection(tauxInf);
             maladie.setTauxGuerison(tauxGue);
             maladie.setTauxMortalite(tauxDead);
+            maladie.setIncubation(incubation);
+            maladie.setImmunitePossible(immunitePossible);
             sauvegarder();
         }
         
